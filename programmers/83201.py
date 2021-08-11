9a39f51b62aa23b7763653092a85ceb16f60bcbd
lelemita <nywfox@gmail.com>
2021-08-11 17:26:04 +0900

    83201: Weekly Challenge: 2nd 두번째 시도 통과

--- programmers/83201.py
+++ programmers/83201.py
@@ -1,15 +1,16 @@
 # Weekly Challenge: 2nd week
 # https://programmers.co.kr/learn/courses/30/lessons/83201?language=python3
-# 첫시도 실패 케이스: 10, 12, 15, 16, 19
+# 첫시도 실패 케이스: 10, 12, 15, 16, 19 - 중복여부를 잘못 적용하고 있었음
+# 두번째 시도 통과
 
 def solution(scores):
     answer = ''
     scores = transpose(scores)
     for idx, scos in enumerate(scores):
-        mini, maxi, total = calculate(scos)
+        mini, maxi, total, is_once = calculate(scos, idx)
         people = len(scos)
         self = scos[idx]
-        if self in (mini, maxi):
+        if is_once and self in (mini, maxi):
             total -= self
             people -= 1
         answer += labeling(total / people)
@@ -24,17 +25,20 @@ def transpose(arrs):
     return result
 
 
-def calculate(scos):
+def calculate(scos, idx):
     total = 0
     mini = 101
     maxi = -1
-    for sco in scos:
+    is_once = True
+    for i, sco in enumerate(scos):
         total += sco
         if mini > sco:
             mini = sco
         if maxi < sco:
             maxi = sco
-    return mini, maxi, total
+        if i != idx and sco == scos[idx]:
+            is_once = False
+    return mini, maxi, total, is_once
 
 
 def labeling(score):
@@ -51,9 +55,9 @@ def labeling(score):
 
 
 if __name__ == "__main__":
-    # assert solution([[100, 90, 98, 88, 65], [50, 45, 99, 85, 77], [47, 88, 95, 80, 67], [61, 57, 100, 80, 65],
-    #                  [24, 90, 94, 75, 65]]) == "FBABD"
-    # assert solution([[50, 90], [50, 87]]) == "DA"
-    # assert solution([[70, 49, 90], [68, 50, 38], [73, 31, 100]]) == "CFD"
-    print(solution([[100, 100, 0], [0, 100, 100], [100, 0, 100]]))
+    assert solution([[100, 90, 98, 88, 65], [50, 45, 99, 85, 77], [47, 88, 95, 80, 67], [61, 57, 100, 80, 65],
+                     [24, 90, 94, 75, 65]]) == "FBABD"
+    assert solution([[50, 90], [50, 87]]) == "DA"
+    assert solution([[70, 49, 90], [68, 50, 38], [73, 31, 100]]) == "CFD"
+    assert solution([[75, 50, 100], [75, 100, 20], [100, 100, 20]]) == "BBF"
     print("done")
